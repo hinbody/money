5c130cfc4ddf14ea724fa4df602564312017e521
Harley Inbody <hinbody@gmail.com>
2013-07-02 21:14:20 -0400

    revise specs for Account as per @marksim's suggestion

--- spec/test_money.rb
+++ spec/test_money.rb
@@ -2,7 +2,6 @@ require "money"
 
 describe Money do
   it "can create a new account" do
-    @account = Account.new "name", "number", "due", "date", "last_amount",
-      "last_date"
+    @account = Account.new "name", "number"
   end
 end
--- spec/test_new_account.rb
+++ spec/test_new_account.rb
@@ -1,8 +1,7 @@
 require 'account'
 describe Account do
   before :each do
-    @account = Account.new "Name", "account_number", "current_due", "due_date",
-      "last_payment_date", "last_payment_amount"
+    @account = Account.new "Name", "account_number"
   end
   describe "#new" do
     it "creates a new account with proper attributes" do
@@ -17,26 +16,6 @@ describe Account do
   describe "#account_number" do
     it "returns the correct number for account" do
       @account.account_number.should eql "account_number"
-    end
-  end
-  describe "#current_due" do
-    it "returns the current amount due for account" do
-      @account.current_due.should eql "current_due"
-    end
-  end
-  describe "#due_date" do
-    it "returns the current date due for current_due" do
-      @account.due_date.should eql "due_date"
-    end
-  end
-  describe "#last_payment_date" do
-    it "returns the date of last payment made" do
-      @account.last_payment_date.should eql "last_payment_date"
-    end
-  end
-  describe "#last_payment_amount" do
-    it "returns the amount of last payment made" do
-      @account.last_payment_amount.should eql "last_payment_amount"
     end
   end
 end
