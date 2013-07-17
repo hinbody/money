0b831b3431cdfb0acf0bcb135f4c4e424e08e8a5
Harley Inbody <hinbody@gmail.com>
2013-07-16 22:08:29 -0400

    add id column to Account class

--- lib/account.rb
+++ lib/account.rb
@@ -2,7 +2,8 @@ require 'data_store'
 require 'bill'
 
 class Account
-  attr_accessor :name, :account_number, :recurring, :bills
+  attr_accessor :id, :name, :account_number, :recurring, :bills
+
   def initialize name, number, recurring=false
     @name = name
     @account_number = number
@@ -10,15 +11,16 @@ class Account
     @bills = []
   end
 
-  def self.create name, account_number
+  def self.create  name, account_number
     a = Account.new(name, account_number)
     a.persist!
+    a.id = db.execute("select last_insert_rowid()")
     a
   end
 
   def self.find_by_name name
     db.execute("select * from Accounts where name='#{name}'") do |row|
-      return Account.new(row[0], row[1], row[2].to_i > 0)
+      return Account.new(row[1], row[2], row[3].to_i > 0)
     end
   end
 
@@ -31,14 +33,15 @@ class Account
 
   def persist!
     # TODO - Extract into "migration" ??
-    db.execute "CREATE TABLE IF NOT EXISTS Accounts(name, account_number, recurring)"
-    db.execute "INSERT INTO Accounts Values(?, ?, ?)", @name, @account_number, @recurring ? 1 : 0
+    db.execute "CREATE TABLE IF NOT EXISTS Accounts(id INTEGER PRIMARY KEY, 
+    name, account_number, recurring)"
+    db.execute "INSERT INTO Accounts Values(NULL, ?, ?, ?)", @name, @account_number, @recurring ? 1 : 0
   rescue SQLite3::Exception => e
     puts "Exception occured"
     puts e
   end
 
-private
+  private
 
   def self.db
     DataStore.instance
