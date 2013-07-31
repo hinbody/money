9d1ade986f56676903f15aa17a3373033643d6bd
Harley Inbody <hinbody@gmail.com>
2013-07-30 20:24:52 -0400

    Add parens to method definitions in Account class and remove unnecessary tests

--- lib/money/account.rb
+++ lib/money/account.rb
@@ -2,28 +2,28 @@ module Money
   class Account
     attr_accessor :id, :name, :account_number, :recurring, :bills
 
-    def initialize name, number, recurring=false
+    def initialize(name, number, recurring=false)
       @name = name
       @account_number = number
       @recurring = recurring
       @bills = []
     end
 
-    def self.create  name, account_number
+    def self.create(name, account_number)
       a = Account.new(name, account_number)
       a.persist!
       a.id = db.execute("select last_insert_rowid()")
       a
     end
 
-    def self.find_by_name name
+    def self.find_by_name(name)
       db.execute("select * from Accounts where name='#{name}'") do |row|
         return Account.new(row[1], row[2], row[3].to_i > 0)
       end
     end
 
 
-    def add_bill due_date, amount, account_id
+    def add_bill(due_date, amount, account_id)
       @bills << Bill.new(due_date, amount, account_id)
       @bills[0].persist!
       @bills
--- spec/test_money.rb
+++
@@ -1,7 +0,0 @@
-require "money"
-
-describe Money do
-  it "can create a new account" do
-    @account = Account.new "name", "number"
-  end
-end
