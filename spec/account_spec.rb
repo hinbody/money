576ec79ce8511dd673c3747c6d33a89e66d5f44c
Harley Inbody <hinbody@gmail.com>
2013-07-31 20:20:33 -0400

    Pull variable assignment out of several specs and put into a before block

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,29 +8,30 @@ module Money
       Account.create("ComCast", "2003003949")
     end
 
+    before(:all) do
+      @a = Account.find_by_name('ComCast')
+    end
+
     it "can be created" do
-      a = Account.find_by_name('ComCast')
-      a.should_not be_nil
-      a.name.should == 'ComCast'
-      a.name.should_not == 'Att'
-      a.account_number.should == '2003003949'
+      @a.should_not be_nil
+      @a.name.should == 'ComCast'
+      @a.name.should_not == 'Att'
+      @a.account_number.should == '2003003949'
     end
 
     context "bills" do
       it "can add a bill" do
-        a = Account.find_by_name('ComCast')
-        a.bills.should be_empty 
-        a.add_bill('2013-07-01', 29.52, a.id)
-        a.bills.should_not be_empty
+        @a.bills.should be_empty 
+        @a.add_bill('2013-07-01', 29.52, @a.id)
+        @a.bills.should_not be_empty
       end
 
       it "has many bills" do
       end
 
       it "can get the first bill" do
-        a = Account.find_by_name('ComCast')
-        a.add_bill("2013-07-01", 29.52, a.id)
-        a.bills.first.should be_a Bill
+        @a.add_bill("2013-07-01", 29.52, @a.id)
+        @a.bills.first.should be_a Bill
       end
     end
   end
